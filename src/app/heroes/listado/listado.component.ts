05dca693af45af599f4a45ba71505df0fcf71279
Ricardo Soto <ricardosoto.saez@gmail.com>
2021-08-23 09:13:59 -0400

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan América'];
+
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    // this.heroes.splice(0, 1);
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+}
